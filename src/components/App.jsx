1f796aeaa76c37d0cdcbf631e7ddd5190e145a6f
Jauco Noordzij <code@jauco.nl>
2016-02-08 15:49:44 +0100

    Fix hyperlinks for menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,11 @@ export default React.createClass({
 					<nav className="navigation">
 						<div className="menu-main-container">
 							<ul id="menu-main" className="menu">
-								<li id="menu-item-32" className="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-32"><a href="/">Home</a></li>
-								<li id="menu-item-21" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-21"><a href="http://database.dutch-caribbean.huygens.knaw.nl/">Search the database</a></li>
-								<li id="menu-item-11" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=10">About the project</a></li>
-								<li id="menu-item-131" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-131"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=128">Introductions database</a></li>
-								<li id="menu-item-132" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-132"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=125">Maps</a></li>
+								<li id="menu-item-32" className="menu-item"><a href="http://dutch-caribbean.huygens.knaw.nl/">Home</a></li>
+								<li id="menu-item-21" className="menu-item"><a href="http://database.dutch-caribbean.huygens.knaw.nl/">Search the database</a></li>
+								<li id="menu-item-11" className="menu-item"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=10">About the project</a></li>
+								<li id="menu-item-131" className="menu-item"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=128">Introductions database</a></li>
+								<li id="menu-item-132" className="menu-item"><a href="http://dutch-caribbean.huygens.knaw.nl/?page_id=125">Maps</a></li>
 							</ul>
 						</div>
 					</nav>
