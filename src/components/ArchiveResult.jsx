7a2a5024d5a1acde2f7c4bcfc7c18d8e6f729693
renevanderark <r.van.der.ark@gmail.com>
2016-11-03 09:20:10 +0100

    removes comment block.

--- src/components/ArchiveResult.jsx
+++ src/components/ArchiveResult.jsx
@@ -14,17 +14,3 @@ export default React.createClass({
     </li>);
   }
 });
-/*
- <span className="ref">{ (r.countries_ss || [""])[0] } {r.refCodeArchive_s} {r.refCode} {r.subCode} {r.itemNo || r.series}</span>
-
- "beginDate": "1816",
- "refCodeArchive": "HaNA",
- "endDate": "1849",
- "subCode": null,
- "series": "3610-3737",
- "_id": "016f16ff-2c80-4cb6-be62-50116577ace3",
- "countries": "NL",
- "itemNo": null,
- "refCode": "2.10.01",
- "titleEng": "\"Government journal of Curaçao\", register of proceedings, decisions, deeds, and letters of the Governor of Curaçao and its dependencies"
- */
